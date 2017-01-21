4e219dfc2559080ab8283fe59f4be3e0791acbb0
Wojciech Czarny <wojtek.black@gmail.com>
2017-01-21 02:42:23 +0100

    Wojtek sync (flames, wobble script, scene)

---
+++ Assets/Scripts/PlantsShake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+    private GameObject[] smilingFoliage;
+
+    public float amplitude;
+    public float offset;
+    public float frequency;
+
+    // Use this for initialization
+    void Start ()
+    {
+        smilingFoliage = GameObject.FindGameObjectsWithTag( "SmilingFoliage" );
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        int index = 0;
+        foreach ( GameObject instance in smilingFoliage )
+        {
+            instance.transform.Translate( new Vector3( 0, Mathf.Sin( Time.time * frequency + offset * index ) * amplitude * Time.deltaTime ) );
+            ++index;
+        }
+    }
+}
