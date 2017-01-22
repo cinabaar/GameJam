328020a6c2a8ce7223ffda883449e735dd1d3fd7
Dawid Romanowski <dawid.t.romanowski@gmail.com>
2017-01-22 10:35:40 +0100

    Main menu and controls

---
+++ Assets/Scripts/ShowNextScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowNextScene : MonoBehaviour {
+    public string nextScene = "Scenes/Wojtek";
+    // Use this for initialization
+
+    private StartOptions start;
+    private bool loading = false;
+
+    void Start ()
+    {
+        var ui = GameObject.Find("UI");
+        if (ui == null) return;
+        start = ui.GetComponent<StartOptions>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown && start!= null && !loading)
+        {
+            loading = true;
+            start.StartButtonClicked(nextScene);
+        }
+	}
+}
