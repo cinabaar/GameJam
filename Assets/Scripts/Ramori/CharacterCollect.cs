6bd8617cf2aaf30a7d75ae182b255c1d98aaf73e
Dawid Romanowski <dawid.t.romanowski@gmail.com>
2017-01-22 14:29:35 +0100

    Build attempt 1

--- Assets/Scripts/Ramori/CharacterCollect.cs
+++ Assets/Scripts/Ramori/CharacterCollect.cs
@@ -1,5 +1,6 @@
 ﻿
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class CharacterCollect : MonoBehaviour
@@ -10,8 +11,21 @@ public class CharacterCollect : MonoBehaviour
     int collected = 0;
     public Text CollectedText;
 
+    Text chicken;
+    Text fish;
+    Text final;
+
     private void Start()
     {
+        var ui = GameObject.Find("UI");
+        if (ui == null) return;
+        var start = ui.GetComponent<ShowPanels>();
+        start.ShowHighscorePanel();
+
+        fish=GameObject.Find("FishPoints").GetComponent<Text>();
+        final= GameObject.Find("FinalScore").GetComponent<Text>();
+        chicken= GameObject.Find("ChickenPoints").GetComponent<Text>();
+        start.HideHighscorePanel();
         if (CollectedText != null)
             CollectedText.text = "0";
 
@@ -25,6 +39,20 @@ public class CharacterCollect : MonoBehaviour
             return;
         var collectable = other.gameObject.GetComponent<CollectableComponent>();
         collected++;
+        try
+        {
+            if (SceneManager.GetActiveScene().name.Contains("Wojtek"))
+            {
+                chicken.text = collected + "";
+                final.text = ((collected + int.Parse(fish.text)) + Random.Range(-5, 5)) + "";
+            }
+            else if (SceneManager.GetActiveScene().name.Contains("Ramori"))
+            {
+                fish.text = collected + "";
+                final.text = ((collected + int.Parse(chicken.text)) + Random.Range(-5, 5)) + "";
+            }
+        }
+        catch { }
         CollectedText.text = collected + "";
         ExplosionManager.Collect(collectable.gameObject);
         Destroy(collectable.gameObject);
